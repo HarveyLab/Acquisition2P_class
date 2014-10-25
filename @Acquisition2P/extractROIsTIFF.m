1725328cbf7ef6d7b17e8ee23c393687757aaa7f
Selmaan <smaaan@gmail.com>
2014-10-24 20:52:27 -0400

    extractROIsTIFF updated to match extractROIs
    Specifically avoids neuropil error by checking to see if field exists,
    and always calculates a raw as well as corrected trace we can use for
    dF/F calculation

--- @Acquisition2P/extractROIsTIFF.m
+++ @Acquisition2P/extractROIsTIFF.m
@@ -1,4 +1,4 @@
-function [traces,roiList,roiMat] = extractROIsTIFF(obj,roiGroups,movNums,sliceNum,channelNum,doSubtraction)
+function [traces,rawF,roiList,roiMat,rawMat] = extractROIsTIFF(obj,roiGroups,movNums,sliceNum,channelNum)
 
 % Function for extracting ROIs from movies using grouping assigned by
 % selectROIs. This function motion corrected tiff files. See extractROI for a
@@ -10,6 +10,7 @@ function [traces,roiList,roiMat] = extractROIsTIFF(obj,roiGroups,movNums,sliceNu
 %traces - matrix of n_cells x n_frames fluorescence values, using neuropil correction for ROIs with a matched neuropil ROI
 %rawF - matrix of same size as traces, but without using neuropil correction
 %roiList - vector of ROI numbers corresponding to extracted ROIs
+%roiMat - Matrix of ROI coefficients
 
 %% Input Handling
 if ~exist('sliceNum','var') || isempty(sliceNum)
@@ -24,9 +25,6 @@ end
 if ~exist('roiGroups','var') || isempty(roiGroups)
     roiGroups = 1:9;
 end
-if ~exist('doSubtraction','var') || isempty(doSubtraction)
-    doSubtraction = 1;
-end
 
 %% ROI Extraction
 
@@ -36,24 +34,30 @@ roiList = find(ismember(roiGroup,roiGroups));
 
 %Construct matrix of ROI values
 roiMat = [];
+rawMat = [];
 for nROI = 1:length(roiList)
     mask = zeros(size(obj.roiInfo.slice(sliceNum).roiLabels));
     cellBodyInd = obj.roiInfo.slice(sliceNum).roi(roiList(nROI)).indBody;
     mask(cellBodyInd) = 1/length(cellBodyInd);
-    if doSubtraction && ~isempty(obj.roiInfo.slice(sliceNum).roi(roiList(nROI)).indNeuropil)
+    maskRaw = mask;
+    if isfield(obj.roiInfo.slice(sliceNum).roi(roiList(nROI)),'indNeuropil')...
+            && ~isempty(obj.roiInfo.slice(sliceNum).roi(roiList(nROI)).indNeuropil)
         neuropilInd = obj.roiInfo.slice(sliceNum).roi(roiList(nROI)).indNeuropil;
         subCoef = obj.roiInfo.slice(sliceNum).roi(roiList(nROI)).subCoef;
         mask(neuropilInd) = -subCoef/length(neuropilInd);
     end
     roiMat(:,end+1) = reshape(mask,[],1);
+    rawMat(:,end+1) = reshape(maskRaw,[],1);
 end
 
 %Loop over movie files, loading each individually and concatenating traces
 traces = [];
+rawF = [];
 for nMovie = movNums
     fprintf('Loading movie %03.0f of %03.0f\n',find(nMovie==movNums),length(movNums)),
     mov = readCor(obj,nMovie,'double');
     mov = reshape(mov,[],size(mov,3));
     traces = cat(2,traces,roiMat'*mov);
+    rawF = cat(2,rawF,rawMat'*mov);
 end
 
