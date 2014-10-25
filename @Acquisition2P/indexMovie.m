8445073cbcc49b6e38767f6ec90bada211dc2879
Selmaan <smaaan@gmail.com>
2014-10-24 20:55:15 -0400

    slice/channel selection for indexMovie
    indexMovie now operates on only a single, specified slice and channel,
    to be broadly comparable with structure of most other methods. Also
    handles missing writeDir input now.

--- @Acquisition2P/indexMovie.m
+++ @Acquisition2P/indexMovie.m
@@ -1,32 +1,34 @@
-function indexMovie(obj,writeDir)
+function indexMovie(obj,nSlice,nChannel,writeDir)
 %Function for creating a single large binary file containing an entire
-%movie for a given slice channel, allowing rapid, indexed access to pixel
+%movie for a given slice/channel, allowing rapid, indexed access to pixel
 %values. Needed to view ROI traces online within selectROIs GUI
 %
 %writeDir is the location to write the binary file to, defaults to defaultDir
 
-if ~exist('writeDir','var')
+%% Input handling
+if ~exist('writeDir','var') || isempty(writeDir)
     writeDir = obj.defaultDir;
 end
 
-nSlices = length(obj.correctedMovies);
-nChannels = length(obj.correctedMovies.slice(1));
-
-for nSlice = 1:nSlices
-    for nChannel = 1:nChannels
-        thisFileName = sprintf('_slice%02.0f_chan%02.0f_mov.bin',nSlice,nChannel);
-        movFileName = fullfile(writeDir,[obj.acqName, thisFileName]);
-        obj.indexedMovie.slice(nSlice).channel(nChannel).fileName = movFileName;
-        fprintf('Saving file %s\n',movFileName),
-        hMovFile = fopen(movFileName,'a');
-        for nMovie = 1:length(obj.correctedMovies.slice(nSlice).channel(nChannel).fileName)
-            fprintf('Saving movie %03.0f of %03.0f\n',nMovie,length(obj.correctedMovies.slice(nSlice).channel(nChannel).fileName)),
-            mov = readCor(obj,nMovie,[],nSlice,nChannel);
-            nFrames = size(mov,3);
-            mov = reshape(mov,[],nFrames);
-            fwrite(hMovFile,mov,'uint16');
-        end
-        fclose(hMovFile);
-    end
+if ~exist('nSlice','var') || isempty(nSlice)
+    nSlice = 1;
 end
-            
+
+if ~exist('nChannel','var') || isempty(nChannel)
+    nChannel = 1;
+end
+
+%% 
+thisFileName = sprintf('_slice%02.0f_chan%02.0f_mov.bin',nSlice,nChannel);
+movFileName = fullfile(writeDir,[obj.acqName, thisFileName]);
+obj.indexedMovie.slice(nSlice).channel(nChannel).fileName = movFileName;
+fprintf('Saving file %s\n',movFileName),
+hMovFile = fopen(movFileName,'a');
+for nMovie = 1:length(obj.correctedMovies.slice(nSlice).channel(nChannel).fileName)
+    fprintf('Saving movie %03.0f of %03.0f\n',nMovie,length(obj.correctedMovies.slice(nSlice).channel(nChannel).fileName)),
+    mov = readCor(obj,nMovie,[],nSlice,nChannel);
+    nFrames = size(mov,3);
+    mov = reshape(mov,[],nFrames);
+    fwrite(hMovFile,mov,'uint16');
+end
+fclose(hMovFile);
