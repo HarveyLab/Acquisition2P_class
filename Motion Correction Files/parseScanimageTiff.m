bc37655459aca8cf1b07f822d3d64c8ae23ed283
Selmaan <chettih@fas.harvard.edu>
2014-11-13 15:36:55 -0500

    Added support for SI3 in parseScanImageTiff
    parse function now checks for signature of SI version in metadata, and
    uses this to appropriately determine movie information

--- Motion Correction Files/parseScanimageTiff.m
+++ Motion Correction Files/parseScanimageTiff.m
@@ -1,15 +1,19 @@
 function [movStruct, nSlices, nChannels] = parseScanimageTiff(mov, siStruct)
 
-% Get rid of additional SI struct layer:
+% Check for scanimage version before extracting metainformation
 if isfield(siStruct, 'SI4')
     siStruct = siStruct.SI4;
+    % Nomenclature: frames and slices refer to the concepts used in
+    % ScanImage.
+    fZ              = siStruct.fastZEnable;
+    nChannels       = numel(siStruct.channelsSave);
+    nSlices         = siStruct.stackNumSlices + (fZ*siStruct.fastZDiscardFlybackFrames); % Slices are acquired at different locations (e.g. depths).
+elseif isfield(siStruct, 'software') && siTruct.software.version < 4 %ie it's a scanimage 3 file
+    fZ = 0;
+    nSlices = 1;
+    nChannels = siStruct.acq.numberOfChannelsSave;
 end
 
-% Nomenclature: frames and slices refer to the concepts used in
-% ScanImage.
-fZ              = siStruct.fastZEnable;
-nChannels       = numel(siStruct.channelsSave);
-nSlices         = siStruct.stackNumSlices + (fZ*siStruct.fastZDiscardFlybackFrames); % Slices are acquired at different locations (e.g. depths).
 
 % Copy data into structure:
 for sl = 1:nSlices-(fZ*siStruct.fastZDiscardFlybackFrames) % Slices, removing flyback.
