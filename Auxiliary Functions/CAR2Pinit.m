24048955e64e1033e478735c6ec4dca103cee403
carolinerunyan <caroline.runyan@gmail.com>
2014-11-14 12:57:26 -0500

    CAR added my initialization file

---
+++ Auxiliary Functions/CAR2Pinit.m
@@ -0,0 +1,43 @@
+function CAR2Pinit(obj)
+%Example of an Acq2P Initialization Function. Allows user selection of
+%movies to form acquisition, sorts alphabetically, assigns an acquisition
+%name and default directory, and assigns the object to a workspace variable
+%named after the acquisition name
+
+%Initialize user selection of multiple tif files
+[movNames, movPath] = uigetfile('*.tif','MultiSelect','on');
+
+%Set default directory to folder location,
+obj.defaultDir = movPath;
+
+%sort movie order alphabetically for consistent results
+movNames = sort(movNames);
+
+%Attempt to automatically name acquisition from movie filename, raise
+%warning and create generic name otherwise
+% acqNamePlace = find(movNames{1} == '0',1,'last');
+% try
+%     obj.acqName = movNames{1}(1:acqNamePlace-1);
+% catch
+%     obj.acqName = sprintf('%s_%.0f',date,now);
+    obj.acqName = input('Object Acquisition Name');
+%     warning('Automatic Name Generation Failed, using date_time')
+% end
+
+%Attempt to add each selected movie to acquisition in order
+for nMov = 1:length(movNames)
+    obj.addMovie(fullfile(movPath,movNames{nMov}));
+end
+
+%Automatically fill in fields for motion correction
+obj.motionRefMovNum = floor(length(movNames)/2);
+obj.motionRefChannel = 2;
+obj.binFactor = 1;
+obj.motionCorrectionFunction = @withinFile_withinFrame_lucasKanade;
+
+
+%Assign acquisition object to acquisition name variable in workspace
+assignin('base',obj.acqName,obj);
+
+%Notify user of success
+fprintf('Successfully added %03.0f movies to acquisition: %s\n',length(movNames),obj.acqName),
