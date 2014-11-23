22101b3809627cff036da230dfd5aa5be85baffc
mjlm <m.minderer@gmail.com>
2014-11-23 14:12:58 -0500

    Added Matthias' initialization function.

--- Auxiliary Functions/Matthias/mjlmInitialization.m
+++ Auxiliary Functions/Matthias/mjlmInitialization.m
@@ -1,17 +1,29 @@
-function mjlmInitialization(acqId)
+function acq = mjlmInitialization(acqId, savePath)
 % acqId (formerly fileId) is some fragment of the file name of the current
 % acquisition that is unique to the current acquisition.
-acq = Acquisition2P(acqId, @init);
 
-function init(acq)
+acq = Acquisition2P(acqId, @(acq) init(acq, acqId));
+
+if exist('savePath', 'var')
+    save(fullfile(savePath, acq.acqName), acq);
+end
+
+function init(acq, acqId)
 % Add remote files:
 acq.Movies = improc.findFiles(acqId, 0, 0);
 
 % Set default dir to local "processed" dir:
-acq.defaultDir = improc.dir.getDir(acqId, 1, 1);
+[~, host] = system('hostname');
+host = strtrim(host);
+switch host
+    case 'Matthias-X230'
+        acq.defaultDir = improc.dir.getDir(acqId, 1, 1);
+    otherwise
+        acq.defaultDir = improc.dir.getDir(acqId, 0, 1);
+end
 
 % Motion correction parameters:
-obj.motionCorrectionFunction = @withinFile_withinFrame_lucasKanade;
-obj.motionRefMovNum = floor(length(movNames)/2);
-obj.motionRefChannel = 1;
-obj.binFactor = 1;
+acq.motionCorrectionFunction = @withinFile_withinFrame_lucasKanade;
+acq.motionRefMovNum = floor(length(acq.Movies)/2);
+acq.motionRefChannel = 1;
+acq.binFactor = 1;
