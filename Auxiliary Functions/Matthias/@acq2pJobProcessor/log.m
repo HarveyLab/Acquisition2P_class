76b393b737b8abf00ddd00f7ea1f12b707bcdaa2
mjlm-carolines-rig <m.minderer@gmail.com>
2014-11-24 10:41:22 -0500

    Matthias: Graceful handling of network issues in jobprocessor.

--- Auxiliary Functions/Matthias/@acq2pJobProcessor/log.m
+++ Auxiliary Functions/Matthias/@acq2pJobProcessor/log.m
@@ -17,10 +17,38 @@ msgFull = sprintf('%s\t%s\t%s\t%s\n', ...
     ajp.currentAcq.acqName, ...
     msg);
 
-% Write to log file:
-fid = fopen(ajp.logFileName, 'a');
-fprintf(fid, '%s', msgFull);
-fclose(fid);
-
 % Write to command line:
 fprintf('%s', msgFull);
+
+% Write to log file:
+success = false;
+try
+    fid = fopen(ajp.logFileName, 'a');
+    success = true;
+catch err
+    if strcmp(err.identifier, 'MATLAB:badfid_mx')
+        warning('Couldn''t open log file. Will try again once, then proceed without logging.');
+        pause(60); % Wait a minute to let network connection reset.
+    else
+        rethrow(err)
+    end
+end
+
+% Try again if writing to log file failed (probably due to intermittent
+% network error).
+if ~success
+    try
+        fid = fopen(ajp.logFileName, 'a');
+    catch err
+        if strcmp(err.identifier, 'MATLAB:badfid_mx')
+            warning('Retry failed. Proceeding without writing to log file.');
+        else
+            rethrow(err)
+        end
+    end
+end
+
+if success
+    fprintf(fid, '%s', msgFull);
+    fclose(fid);
+end
