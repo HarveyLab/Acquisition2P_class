bfa4ad9234d305f215de96a1e9de1de2757b726c
Ari Morcos <arimorcos@gmail.com>
2014-11-26 10:59:09 -0500

    Added ability to specify custom naming function
    Can now specify custom naming function as fourth argument to
    motionCorrect. namingFunction is a handle to a function for naming. If
    empty, uses default naming function which is a local function of
    motionCorrect. All naming functions must take in the following arguments
    (in  order): obj.acqName, nSlice, nChannel, movNum.

--- @Acquisition2P/motionCorrect.m
+++ @Acquisition2P/motionCorrect.m
@@ -1,110 +1,131 @@
-function motionCorrect(obj,writeDir,motionCorrectionFunction)
-    %Generic wrapper function for managing motion correction of an
-    %acquisition object
-    %
-    %motionCorrect(obj,writeDir,motionCorrectionFunction)
-    %
-    %writeDir is an optional argument specifying location to write motion
-    %   corrected data to, defaults to obj.defaultDir\Corrected
-    %motionCorrectionFunction is a handle to a motion correction function,
-    %   and is optional only if acquisition already has a function handle assigned 
-    %   to motionCorectionFunction field. If argument is provided, function handle overwrites field in acq obj.
-    
-%% Error checking and input handling    
-    if ~exist('motionCorrectionFunction', 'var')
-        motionCorrectionFunction = [];
-    end
-    
-    if isempty(motionCorrectionFunction) && isempty(obj.motionCorrectionFunction)
-        error('Function for correction not provided as argument or specified in acquisition object');
-    elseif isempty(motionCorrectionFunction)
-        %If no argument but field present for object, use that function
-        motionCorrectionFunction = obj.motionCorrectionFunction;
-    else
-        %If using argument provided, assign to obj field
-        obj.motionCorrectionFunction = motionCorrectionFunction;
-    end
-    
-    if isempty(obj.acqName)
-        error('Acquisition Name Unspecified'),
-    end
-        
-    if ~exist('writeDir', 'var') || isempty(writeDir) %Use Corrected in Default Directory if non specified
-        if isempty(obj.defaultDir)
-            error('Default Directory unspecified'),
-        else
-            writeDir = [obj.defaultDir filesep 'Corrected'];
-        end
-    end
-    
+function motionCorrect(obj,writeDir,motionCorrectionFunction,namingFunction)
+%Generic wrapper function for managing motion correction of an
+%acquisition object
+%
+%motionCorrect(obj,writeDir,motionCorrectionFunction)
+%
+%writeDir is an optional argument specifying location to write motion
+%   corrected data to, defaults to obj.defaultDir\Corrected
+%motionCorrectionFunction is a handle to a motion correction function,
+%   and is optional only if acquisition already has a function handle
+%   assigned to motionCorectionFunction field. If argument is provided,
+%   function handle overwrites field in acq obj.
+%namingFunction is a handle to a function for naming. If empty, uses
+%   default naming function which is a local function of motionCorrect.
+%   All naming functions must take in the following arguments (in
+%   order): obj.acqName, nSlice, nChannel, movNum.
+
+%% Error checking and input handling
+if ~exist('motionCorrectionFunction', 'var')
+    motionCorrectionFunction = [];
+end
+
+if nargin < 4 || isempty(namingFunction)
+    namingFunction = @defaultNamingFunction;
+end
+
+if isempty(motionCorrectionFunction) && isempty(obj.motionCorrectionFunction)
+    error('Function for correction not provided as argument or specified in acquisition object');
+elseif isempty(motionCorrectionFunction)
+    %If no argument but field present for object, use that function
+    motionCorrectionFunction = obj.motionCorrectionFunction;
+else
+    %If using argument provided, assign to obj field
+    obj.motionCorrectionFunction = motionCorrectionFunction;
+end
+
+if isempty(obj.acqName)
+    error('Acquisition Name Unspecified'),
+end
+
+if ~exist('writeDir', 'var') || isempty(writeDir) %Use Corrected in Default Directory if non specified
     if isempty(obj.defaultDir)
-        obj.defaultDir = writeDir;
+        error('Default Directory unspecified'),
+    else
+        writeDir = [obj.defaultDir filesep 'Corrected'];
     end
-    
-    if isempty(obj.motionRefMovNum)
-        if length(obj.Movies)==1
-            obj.motionRefMovNum = 1;
-        else
-            error('Motion Correction Reference not identified');
-        end
+end
+
+if isempty(obj.defaultDir)
+    obj.defaultDir = writeDir;
+end
+
+if isempty(obj.motionRefMovNum)
+    if length(obj.Movies)==1
+        obj.motionRefMovNum = 1;
+    else
+        error('Motion Correction Reference not identified');
     end
-    
+end
+
 %% Load movies and motion correct
-    %Calculate Number of movies and arrange processing order so that reference is first
-    nMovies = length(obj.Movies);
-    if isempty(obj.motionRefMovNum)
-        obj.motionRefMovNum = floor(nMovies/2);
+%Calculate Number of movies and arrange processing order so that
+%reference is first
+nMovies = length(obj.Movies);
+if isempty(obj.motionRefMovNum)
+    obj.motionRefMovNum = floor(nMovies/2);
+end
+movieOrder = 1:nMovies;
+movieOrder([1 obj.motionRefMovNum]) = [obj.motionRefMovNum 1];
+
+%Load movies one at a time in order, apply correction, and save as
+%split files (slice and channel)
+for movNum = movieOrder
+    fprintf('\nLoading Movie #%03.0f of #%03.0f\n',movNum,nMovies),
+    [mov, scanImageMetadata] = obj.readRaw(movNum,'single');
+    if obj.binFactor > 1
+        mov = binSpatial(mov, obj.binFactor);
     end
-    movieOrder = 1:nMovies;
-    movieOrder([1 obj.motionRefMovNum]) = [obj.motionRefMovNum 1];
     
-    %Load movies one at a time in order, apply correction, and save as
-    %split files (slice and channel)
-    for movNum = movieOrder
-        fprintf('\nLoading Movie #%03.0f of #%03.0f\n',movNum,nMovies),
-        [mov, scanImageMetadata] = obj.readRaw(movNum,'single');
-        if obj.binFactor > 1
-            mov = binSpatial(mov, obj.binFactor);
-        end
-        
-        % Apply line shift:
-        fprintf('Line Shift Correcting Movie #%03.0f of #%03.0f\n', movNum, nMovies),
-        mov = correctLineShift(mov);
-        try
-            [movStruct, nSlices, nChannels] = parseScanimageTiff(mov, scanImageMetadata);
-        catch
-            error('parseScanimageTiff failed to parse metadata, likely non SI4 movie, modify function!'),
-        end
-        clear mov
-        
-        % Find motion:
-        fprintf('Identifying Motion Correction for Movie #%03.0f of #%03.0f\n', movNum, nMovies),
-        obj.motionCorrectionFunction(obj, movStruct, scanImageMetadata, movNum, 'identify');
-        
-        % Apply motion correction and write separate file for each slice\channel:
-        fprintf('Applying Motion Correction for Movie #%03.0f of #%03.0f\n', movNum, nMovies),
-        movStruct = obj.motionCorrectionFunction(obj, movStruct, scanImageMetadata, movNum, 'apply');
-        for nSlice = 1:nSlices
-            for nChannel = 1:nChannels
-                movFileName = sprintf('%s_Slice%02.0f_Channel%02.0f_File%03.0f.tif', obj.acqName, nSlice, nChannel, movNum);
-                obj.correctedMovies.slice(nSlice).channel(nChannel).fileName{movNum} = fullfile(writeDir,movFileName);
-                fprintf('Writing Movie #%03.0f of #%03.0f\n',movNum,nMovies),
-                try
-                    tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'int16');
-                catch
-                    % Sometimes, disk access fails due to intermittent
-                    % network problem. In that case, wait and re-try once:
-                    pause(60);
-                    tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'int16');
-                end
+    % Apply line shift:
+    fprintf('Line Shift Correcting Movie #%03.0f of #%03.0f\n', movNum, nMovies),
+    mov = correctLineShift(mov);
+    try
+        [movStruct, nSlices, nChannels] = parseScanimageTiff(mov, scanImageMetadata);
+    catch
+        error('parseScanimageTiff failed to parse metadata, likely non SI4 movie, modify function!'),
+    end
+    clear mov
+    
+    % Find motion:
+    fprintf('Identifying Motion Correction for Movie #%03.0f of #%03.0f\n', movNum, nMovies),
+    obj.motionCorrectionFunction(obj, movStruct, scanImageMetadata, movNum, 'identify');
+    
+    % Apply motion correction and write separate file for each
+    % slice\channel:
+    fprintf('Applying Motion Correction for Movie #%03.0f of #%03.0f\n', movNum, nMovies),
+    movStruct = obj.motionCorrectionFunction(obj, movStruct, scanImageMetadata, movNum, 'apply');
+    for nSlice = 1:nSlices
+        for nChannel = 1:nChannels
+            movFileName = feval(namingFunction,obj.acqName, nSlice, nChannel, movNum);
+            obj.correctedMovies.slice(nSlice).channel(nChannel).fileName{movNum} = fullfile(writeDir,movFileName);
+            fprintf('Writing Movie #%03.0f of #%03.0f\n',movNum,nMovies),
+            try
+                tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'int16');
+            catch
+                % Sometimes, disk access fails due to intermittent
+                % network problem. In that case, wait and re-try once:
+                pause(60);
+                tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'int16');
             end
         end
-        
-        % Store movie dimensions (this is the same for all channels and slices):
-        obj.derivedData(movNum).size = size(movStruct.slice(nSlice).channel(nChannel).mov);
     end
     
-    %Assign acquisition to a variable with its own name, and write to same directory
-    eval([obj.acqName ' = obj;']),
-    save(fullfile(obj.defaultDir, obj.acqName), obj.acqName)
-    display('Motion Correction Completed!')
+    % Store movie dimensions (this is the same for all channels and
+    % slices):
+    obj.derivedData(movNum).size = size(movStruct.slice(nSlice).channel(nChannel).mov);
+end
+
+%Assign acquisition to a variable with its own name, and write to same
+%directory
+eval([obj.acqName ' = obj;']),
+save(fullfile(obj.defaultDir, obj.acqName), obj.acqName)
+display('Motion Correction Completed!')
+
+end
+
+function movFileName = defaultNamingFunction(acqName, nSlice, nChannel, movNum)
+
+movFileName = sprintf('%s_Slice%02.0f_Channel%02.0f_File%03.0f.tif',...
+    acqName, nSlice, nChannel, movNum);
+end
