9ea6bdc175bd2d59e3ccddc1f58ce898c97349cc
Ari Morcos <arimorcos@gmail.com>
2014-12-09 12:10:37 -0500

    Moved job processor out of Matthias' folder

---
+++ Auxiliary Functions/@acq2pJobProcessor/loadNextAcq.m
@@ -0,0 +1,29 @@
+function success = loadNextAcq(ajp)
+% Checks if there are new unprocessed acq2ps in the to-be-processed folder.
+
+acqFileList = dir(fullfile(ajp.dir.jobs, '*.mat'));
+
+% Go to next un-done job:
+if isempty(acqFileList)
+    success = false;
+    return
+end
+ajp.currentAcqFileName = acqFileList(1).name;
+nextAcqFile = fullfile(ajp.dir.jobs, ajp.currentAcqFileName);
+
+% Load next acquisition:
+acq = load(nextAcqFile); % Load into structure in case variable has weird name.
+name = fieldnames(acq);
+ajp.currentAcq = acq.(name{1});
+
+% Move acq file right away to inProgress directory:
+if ~exist(ajp.dir.inProgress, 'dir');
+    mkdir(ajp.dir.inProgress);
+end
+movefile(nextAcqFile, fullfile(ajp.dir.inProgress, ajp.currentAcqFileName));
+
+% Log information:
+msg = sprintf('Loaded acq2p for processing and moved file to "done" folder: %s.\n', nextAcqFile);
+ajp.log(msg);
+
+success = true;
---
+++ Auxiliary Functions/@acq2pJobProcessor/stop.m
@@ -0,0 +1,4 @@
+function stop(ajp)
+% Causes job processor to stop looking for new jobs.
+
+ajp.flagStop = true;
