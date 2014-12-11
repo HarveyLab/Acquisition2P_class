d4f25a72841ab76a1a7f7e39abeb63452c46691f
mjlm-tarynspc <m.minderer@gmail.com>
2014-12-11 15:54:35 -0500

    paralell IO dev

--- @Acquisition2P/motionCorrect.m
+++ @Acquisition2P/motionCorrect.m
@@ -72,30 +72,39 @@ movieOrder([1 obj.motionRefMovNum]) = [obj.motionRefMovNum 1];
 %split files (slice and channel)
 for m = 1:nMovies
     %Load movie:
-    if exist('parObjRead', 'var')
-        % A parObjRead was created in a previous iteration, so we can
-        % simply retrieve the pre-loaded movie:
-        fprintf('\nRetrieving pre-loaded movie #%03.0f of #%03.0f\n',movieOrder(m),nMovies)
-        [mov, scanImageMetadata] = fetchOutputs(parObjRead);
-        delete(parObjRead); % Necessary to delete data on parallel worker.
+    if exist('parallelIo', 'var')
+        % A parallelIo object was created in a previous iteration, so we
+        % can simply retrieve the pre-loaded movie:
+        fprintf('\nRetrieving pre-loaded movie %1.0f of %1.0f...\n',movieOrder(m),nMovies)
+        [~, mov, scanImageMetadata] = fetchNext(parallelIo);
+        delete(parallelIo); % Delete data on parallel worker.
     else
-        % No parObjRead exists, so this is either the first movie or
+        % No parallelIo exists, so this is either the first movie or
         % pre-loading is switched off, so we load the movie conventionally.
-        fprintf('\nLoading Movie #%03.0f of #%03.0f\n',movieOrder(m),nMovies)
-        distTimer = tic;
+        fprintf('\nLoading movie %1.0f of %1.0f\n',movieOrder(m),nMovies)
+        ticLoad = tic;
         [mov, scanImageMetadata] = obj.readRaw(movieOrder(m),'single');
-        fprintf('Done loading (%1.0f s).\n', toc(distTimer));
+        fprintf('Done loading (%1.0f s).\n', toc(ticLoad));
     end
     
-    % Re-start parallel pool. This is necessary to counteract memory leaks:
-    fprintf('Re-starting parallel pool to clear leaked memory (this is a known Matlab bug):\n');
+    % Re-start parallel pool. This is necessary to counteract a memory leak
+    % in the parallel workers (this is an issue known to MathWorks): (This
+    % will not pre-maturely abort parallelIo because the fetchNext blocks
+    % Matlab until the parfeval finishes.)
+    fprintf('Re-starting parallel pool to clear leaked memory:\n');
     delete(gcp);
     parpool;
     
-    % Start parallel loading of next movie:
-    if m<nMovies && getFreeMem > 3000
-        isSilent = true;
-        parObjRead = parfeval(@obj.readRaw, 2, movieOrder(m+1), 'single', isSilent);
+    % Start parallel I/O job: This saves the movStruct from the previous
+    % iteration and loads the mov for the next iteration:
+    if m==1
+        % First iteration: no movStruct exists yet that would have to be
+        % saved:
+        parallelIo = parfeval(@ioFun, 2, ...
+            obj, movieOrder, m);
+    else
+        parallelIo = parfeval(@ioFun, 2, ...
+            obj, movieOrder, m, movStruct, writeDir, namingFunction);
     end
     
     % Spatial binning:
@@ -104,57 +113,40 @@ for m = 1:nMovies
     end
     
     % Apply line shift:
-    fprintf('Line Shift Correcting Movie #%03.0f of #%03.0f\n', movieOrder(m), nMovies),
+    fprintf('Line Shift Correcting Movie %1.0f of %1.0f\n', movieOrder(m), nMovies),
     mov = correctLineShift(mov);
     try
-        [movStruct, nSlices, nChannels] = parseScanimageTiff(mov, scanImageMetadata);
+        movStruct = parseScanimageTiff(mov, scanImageMetadata);
     catch
         error('parseScanimageTiff failed to parse metadata, likely non SI4 movie, modify function!'),
     end
     clear mov
     
     % Find motion:
-    fprintf('Identifying Motion Correction for Movie #%03.0f of #%03.0f\n', movieOrder(m), nMovies),
+    fprintf('Identifying Motion Correction for Movie %1.0f of %1.0f\n', movieOrder(m), nMovies),
     obj.motionCorrectionFunction(obj, movStruct, scanImageMetadata, movieOrder(m), 'identify');
     
-    % Apply motion correction and write separate file for each
-    % slice\channel:
-    fprintf('Applying Motion Correction for Movie #%03.0f of #%03.0f\n', movieOrder(m), nMovies),
+    % Apply motion correction:
+    fprintf('Applying Motion Correction for Movie %1.0f of %1.0f\n', movieOrder(m), nMovies),
     movStruct = obj.motionCorrectionFunction(obj, movStruct, scanImageMetadata, movieOrder(m), 'apply');
-    for nSlice = 1:nSlices
-        for nChannel = 1:nChannels
-            movFileName = feval(namingFunction,obj.acqName, nSlice, nChannel, movieOrder(m));
-            obj.correctedMovies.slice(nSlice).channel(nChannel).fileName{movieOrder(m)} = fullfile(writeDir,movFileName);
-            fprintf('Writing Movie #%03.0f of #%03.0f\n',movieOrder(m),nMovies)
-            
-            if exist('parObjWrite', 'var')
-                wait(parObjWrite); % Make sure it's not deleated before it's done.
-                delete(parObjWrite);
-            end
-            
-            parObjWrite = parfeval(@tiffWrite, 0, ...
-                movStruct.slice(nSlice).channel(nChannel).mov, ...
-                movFileName, ...
-                writeDir, ...
-                'int16', ...
-                true);
-            
-            if getFreeMem < 3000
-                disp('Too little free memory...waiting for write.')
-                wait(parObjWrite); % Make sure it's not deleated before it's done.
-                delete(parObjWrite);
-            end
-            
-        end
+     
+    % Save as separate tiff file for each slice\channel:
+    if m==nMovies
+        % Only the last movie is saved here. All other movies are saved
+        % in the background, using parfeval, so that calculations can
+        % proceed during slow I/O (the code for this is above, in the
+        % beginning of the for-loop).
+        saveMovStruct(movStruct, writeDir, namingFunction, obj.acqName, movieOrder(m));
     end
     
     % Store movie dimensions (this is the same for all channels and
     % slices):
-    obj.derivedData(movieOrder(m)).size = size(movStruct.slice(nSlice).channel(nChannel).mov);
+    obj.derivedData(movieOrder(m)).size = size(movStruct.slice(1).channel(1).mov);
 end
 
 % Clean up data on parallel workers:
-delete(parObjWrite)
+wait(parallelIo)
+delete(parallelIo)
 
 %Assign acquisition to a variable with its own name, and write to same
 %directory
@@ -165,7 +157,47 @@ display('Motion Correction Completed!')
 end
 
 function movFileName = defaultNamingFunction(acqName, nSlice, nChannel, movNum)
-
 movFileName = sprintf('%s_Slice%02.0f_Channel%02.0f_File%03.0f.tif',...
     acqName, nSlice, nChannel, movNum);
+end
+     
+function saveMovStruct(movStruct, writeDir, namingFunction, acqName, movNum)
+nSlices = numel(movStruct.slice);
+nChannels = numel(movStruct.slice(1).channel);
+for nSlice = 1:nSlices
+    for nChannel = 1:nChannels
+        movFileName = feval(namingFunction, acqName, nSlice, nChannel, movNum);
+
+        tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, ...
+            movFileName, ...
+            writeDir, ...
+            'int16', ...
+            true);
+    end
+end
+end
+
+function [mov, siStruct] = ioFun(obj, movieOrder, m, movStruct, writeDir, namingFunction)
+% [mov, siStruct] = ioFun(obj, movieOrder, m, movStruct, writeDir, namingFunction)
+% This function handles thes disk input/output for one iteration of the
+% motion correction loop. By executing this function with parfeval, all I/O
+% can be done in the background, while
+
+% Load movie for next iteration:
+if m<numel(movieOrder)
+    [mov, siStruct] = obj.readRaw(movieOrder(m+1), 'single', true);
+else
+    % There's nothing to be loaded if we're at the last movie in the
+    % movieOrder:
+    mov = [];
+    siStruct = [];
+end
+
+% Stop here if there is nothing to be saved:
+if m==1
+    return
+end
+
+% Save movStruct from last iteration:
+saveMovStruct(movStruct, writeDir, namingFunction, obj.acqName, movieOrder(m-1));
 end
