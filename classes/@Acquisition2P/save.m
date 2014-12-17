d23a30d5ca7ce6d26593e45273a22dec1b7b1b82
Selmaan <chettih@fas.harvard.edu>
2014-12-17 17:48:21 -0500

    Added save method to Acq2P class
    Simplifies saving an acq object by overloading save. Should never be
    accidentally called, because standard save command requires a string
    input whereas method requires object input. By default uses the
    defaultDir as write directory and the acqName as both saved file name
    and as the variable the acq is internally represented by, but all
    arguments can be modified

---
+++ classes/@Acquisition2P/save.m
@@ -0,0 +1,26 @@
+function save(obj,writeDir,writeName,varName)
+% Saves the acquisition object. By default uses defaultDir as writeDir and 
+% acqName as both writeName and varName
+% 
+% save(obj,writeDir,writeName,varName)
+%
+% writeDir is the directory to save the acq in
+% writeName is the filename to save the acq as
+% varName is the variable name the acq is represented by within the mat file
+
+if ~exist('writeDir','var') || isempty(writeDir)
+    writeDir = obj.defaultDir;
+end
+
+if ~exist('writeName','var') || isempty(writeName)
+    writeName = obj.acqName;
+end
+
+if ~exist('varName','var') || isempty(varName)
+    varName = obj.acqName;
+end
+
+eval([varName ' = obj;']),
+save(fullfile(writeDir,writeName),varName)
+
+end
