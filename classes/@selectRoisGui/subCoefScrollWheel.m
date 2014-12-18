5dfab17fda3cf15303708d9959923bc912181ecd
Ari Morcos <arimorcos@gmail.com>
2014-12-18 11:56:21 -0500

    Control increases speed of neuropil fit change
    Hold down control while changing neuropil fit to increase speed.

--- classes/@selectRoisGui/subCoefScrollWheel.m
+++ classes/@selectRoisGui/subCoefScrollWheel.m
@@ -2,11 +2,17 @@ function subCoefScrollWheel(sel, ~, evt)
 %Allows interactive adjustment of neuropil subtractive coefficient
 
 
+if strcmpi(evt.Source.CurrentModifier,'control')
+    gain = 0.05;
+else
+    gain = 0.005;
+end
+
 switch sign(evt.VerticalScrollCount)
     case -1 % Scrolling up
-        sel.disp.neuropilCoef(2) = sel.disp.neuropilCoef(2) + .01;
+        sel.disp.neuropilCoef(2) = sel.disp.neuropilCoef(2) + gain;
     case 1 % Scrolling down
-        sel.disp.neuropilCoef(2) = sel.disp.neuropilCoef(2) - .01;
+        sel.disp.neuropilCoef(2) = sel.disp.neuropilCoef(2) - gain;
 end
 
 plotSubScatterFit(sel),
