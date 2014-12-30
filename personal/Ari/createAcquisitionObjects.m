ae5c6d799cf853e4e8412db34af312d238ba8c1f
Ari Morcos <arimorcos@gmail.com>
2014-12-30 16:40:43 -0500

    Random changes to personal stuff

--- personal/Ari/createAcquisitionObjects.m
+++ personal/Ari/createAcquisitionObjects.m
@@ -3,7 +3,7 @@ function createAcquisitionObjects(folder)
 %objects to be processed
 %
 %INPUTS
-%folder - folder to look through 
+%folder - folder to look through
 %
 %ASM 11/14
 
@@ -11,11 +11,11 @@ if nargin < 1 || isempty(folder)
     folder = '\\research.files.med.harvard.edu\Neurobio\HarveyLab\Ari\2P Data\ResScan';
 end
 
-%get subdirectories which match mouseName 
+%get subdirectories which match mouseName
 subDir = getSubDir(folder);
 mouseDir = subDir(~cellfun(@isempty,regexp(subDir,'[A-Z]{2}\d\d\d$')));
 
-%if no mouseDir, check for dateDir 
+%if no mouseDir, check for dateDir
 if isempty(mouseDir)
     dateDir = subDir(~cellfun(@isempty,regexp(subDir,'\d{6}$')));
     if isempty(dateDir) %if no date directories, invalid directory, throw error
@@ -32,7 +32,7 @@ nMouseDir = length(mouseDir);
 %loop through each mouse directory
 for mouseInd = 1:nMouseDir
     
-    %get list of date directories 
+    %get list of date directories
     mouseSubDir = getSubDir(mouseDir{mouseInd});
     dateDir = mouseSubDir(~cellfun(@isempty,regexp(mouseSubDir,'\d{6}$')));
     
@@ -41,46 +41,48 @@ for mouseInd = 1:nMouseDir
     %loop through each date directory and create object
     for dateInd = 1:length(dateDir)
         
-        %get acqName 
+        %get acqName
         fileParts = explode(dateDir{dateInd},filesep);
         acqName = sprintf('%s_%s',fileParts{end-1},fileParts{end});
         acqPath = sprintf('%s%s%s_acq.mat',dateDir{dateInd},filesep,acqName);
         acqStatusPath = sprintf('%s%s%s_status.txt',dateDir{dateInd},filesep,acqName);
         
+        if exist(acqStatusPath,'file')
+            
+            %                 obj = load(acqPath);
+            %
+            %                 neighborhoodConstant = 1.5;
+            %                 if isfield(obj.(acqName).derivedData(1),'SIData')
+            %                     if isfield(obj.(acqName).derivedData(1).SIData,'SI4')
+            %                         objectiveMag = 25;
+            %                         zoomFac = obj.(acqName).derivedData(1).SIData.SI4.scanZoomFactor;
+            %                     elseif isfield(obj.(acqName).derivedData(1).SIData,'SI5')
+            %                         objectiveMag = 16;
+            %                         zoomFac = obj.(acqName).derivedData(1).SIData.SI5.zoomFactor;
+            %                     end
+            %                     pxCovRad = round(objectiveMag*zoomFac/neighborhoodConstant);
+            %                 else
+            %                     pxCovRad = [];
+            %                 end
+            %
+            %                 if obj.(acqName).roiInfo.slice(1).covFile.nh ~= (pxCovRad*2 + 1)
+            %                     delete(acqStatusPath);
+            %                     fprintf('Deleted %s\n',acqPath);
+            %                     obj.(acqName).roiInfo = [];
+            %                     shouldProcFolder = '\\research.files.med.harvard.edu\Neurobio\HarveyLab\Ari\2P Data\ResScan\Acq2PToProcess';
+            %                     eval(sprintf('%s=obj;',acqName));
+            %                     save(sprintf('%s%s%s_acq.mat',shouldProcFolder,filesep,acqName),acqName);
+            %                 end
+            
+            
+            continue;
+        end
+        
         %check if acquisition object exists
         if exist(acqPath,'file')
             
             %check if complete txt exists
-            if exist(acqStatusPath,'file')
-                
-%                 obj = load(acqPath);
-%                 
-%                 neighborhoodConstant = 1.5;
-%                 if isfield(obj.(acqName).derivedData(1),'SIData')
-%                     if isfield(obj.(acqName).derivedData(1).SIData,'SI4')
-%                         objectiveMag = 25;
-%                         zoomFac = obj.(acqName).derivedData(1).SIData.SI4.scanZoomFactor;
-%                     elseif isfield(obj.(acqName).derivedData(1).SIData,'SI5')
-%                         objectiveMag = 16;
-%                         zoomFac = obj.(acqName).derivedData(1).SIData.SI5.zoomFactor;
-%                     end
-%                     pxCovRad = round(objectiveMag*zoomFac/neighborhoodConstant);
-%                 else
-%                     pxCovRad = [];
-%                 end
-%                 
-%                 if obj.(acqName).roiInfo.slice(1).covFile.nh ~= (pxCovRad*2 + 1)
-%                     delete(acqStatusPath);
-%                     fprintf('Deleted %s\n',acqPath);
-%                     obj.(acqName).roiInfo = [];
-%                     shouldProcFolder = '\\research.files.med.harvard.edu\Neurobio\HarveyLab\Ari\2P Data\ResScan\Acq2PToProcess';
-%                     eval(sprintf('%s=obj;',acqName));
-%                     save(sprintf('%s%s%s_acq.mat',shouldProcFolder,filesep,acqName),acqName);
-%                 end
-                
-                
-                continue;
-            end
+            
             
             %load in object
             obj = load(acqPath);
@@ -96,10 +98,10 @@ for mouseInd = 1:nMouseDir
             
         end
         
-        %Create object 
+        %Create object
         Acquisition2P([],{@am2PInit,[],dateDir{dateInd}});
-
-    end 
+        
+    end
     
 end
 
@@ -114,7 +116,7 @@ dirList = {dirList(:).name};
 %filter out movement dots
 dirList = dirList(~ismember(dirList,{'.','..'}));
 
-%append full path to dir 
+%append full path to dir
 dirList = cellfun(@(x) sprintf('%s%s%s',folder,filesep,x),dirList,'UniformOutput',false);
 
 %filter out non-directories
