af07f1de77ae90dbec6883d8f485006368bcd8d0
mjlm-office <m.minderer@gmail.com>
2015-05-01 15:58:56 -0400

    made fftalign variable names more sensible

--- common/Motion Correction Files/lucas-kanade/fftalign.m
+++ common/Motion Correction Files/lucas-kanade/fftalign.m
@@ -1,15 +1,21 @@
-function [u,v] = fftalign(A,B)
+function [u,v] = fftalign(ref, img)
 % [u,v] = fftalign(A,B)
 % From http://scanbox.wordpress.com/2014/03/20/recursive-image-alignment-and-statistics/
-N = min(size(A));
+
+% Make sure images have even width and height:
+ref = ref(1:end-mod(end, 2), 1:end-mod(end, 2));
+img = img(1:end-mod(end, 2), 1:end-mod(end, 2));
+
+N = min(size(ref));
+
+% Make images square:
+yidx = round(size(ref,1)/2)-N/2 + 1 : round(size(ref,1)/2)+ N/2;
+xidx = round(size(ref,2)/2)-N/2 + 1 : round(size(ref,2)/2)+ N/2;
  
-yidx = round(size(A,1)/2)-N/2 + 1 : round(size(A,1)/2)+ N/2;
-xidx = round(size(A,2)/2)-N/2 + 1 : round(size(A,2)/2)+ N/2;
+ref = ref(yidx,xidx);
+img = img(yidx,xidx);
  
-A = A(yidx,xidx);
-B = B(yidx,xidx);
- 
-C = fftshift(real(ifft2(fft2(A).*fft2(rot90(B,2)))));
+C = fftshift(real(ifft2(fft2(ref).*fft2(rot90(img,2)))));
 [~,i] = max(C(:));
 [ii, jj] = ind2sub(size(C),i);
  
