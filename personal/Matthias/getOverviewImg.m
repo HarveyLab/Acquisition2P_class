d3e82fd27a1c33f54cc275be601f3e9a3abe5990
mjlm-office <m.minderer@gmail.com>
2015-10-12 12:45:40 -0400

    Personal changes Matthias

--- personal/Matthias/getOverviewImg.m
+++ personal/Matthias/getOverviewImg.m
@@ -1,11 +1,26 @@
 function img = getOverviewImg(acq)
 % Merges the anatomical and activity images into a nice overview image for
 % selectRois
-img = acq.meanRef;
-img(img<0) = 0;
-img(isnan(img)) = 0;
-img = sqrt(img);
-img = adapthisteq(img/max(img(:)));
-img = repmat(img, 1, 1, 3);
+
+%% Get mean img:
+mode = 'standard';
+
+switch mode
+    case 'standard'    
+        meanImg = acq.meanRef;
+        meanImg(meanImg<0) = 0;
+        meanImg(isnan(meanImg)) = 0;
+        meanImg = sqrt(meanImg);
+        meanImg = adapthisteq(meanImg/max(meanImg(:)));
+        meanImg = repmat(meanImg, 1, 1, 3);
+        
+    case 'high contrast'
+        meanImg = mat2gray(max(acq.meanRef([],[],[],0), [], 3), [0 500]);
+        meanImg = adapthisteq(meanImg, 'clipLimit', 0.005, 'numTiles', [20 20]);
+end
+
+%% Get activity img:
 actImg = imadjust(mat2gray(acq.roiInfo.slice(1).covFile.activityImg));
-img(:,:,1) = 0.7*img(:,:,2)+0.3*actImg;
+
+
+img(:,:,1) = 0.7*meanImg(:,:,2)+0.3*actImg;
--- personal/Matthias/mjlmInitialization.m
+++ personal/Matthias/mjlmInitialization.m
@@ -14,7 +14,7 @@ acqName = regexprep(acqName, '_+', '_');
 % Trim trailing underscores:
 acqName = regexprep(acqName, '_$', '');
 
-acq = Acquisition2P(acqName, @(acq) init(acq, acqId));
+acq = Acquisition2P(acqName, @(acq) init(acq, acqId, savePath));
 
 if exist('savePath', 'var')
     save(fullfile(savePath, acq.acqName), 'acq');
@@ -22,23 +22,33 @@ end
 
 fprintf('Successfully created acquisition %s with %d movies.\n', acq.acqName, numel(acq.Movies));
 
-function init(acq, acqId)
+function init(acq, acqId, savePath)
 % Add remote files:
-acq.Movies = improc.findFiles(acqId, '\\User1-PC\D\data\Matthias', 0);
+
+% Check which rig this acq will be processed on, by looking at the path
+% where the job file will be saved:
+if ~isempty(strfind(savePath, 'jobsToDoScopeRig'))
+     acq.Movies = improc.findFiles(acqId, '\\User1-PC\D\data\Matthias', 0);
+     host = 'scopeRig';
+elseif ~isempty(strfind(savePath, 'jobsToDoKristenPc'))
+    acq.Movies = improc.findFiles(acqId, '\\user-pc\C\data\Matthias\imaging\raw', 0);
+    host = 'kristenPc';
+end
 
 % Set default dir to local "processed" dir:
-[~, host] = system('hostname');
-host = strtrim(host);
 switch host
-%     case 'Matthias-X230'
-%         acq.defaultDir = improc.dir.getDir(acqId, 1, 1);
-    otherwise
-%         acq.defaultDir = improc.dir.getDir(acqId, 0, 1);
+    case 'Matthias-X230'
+        acq.defaultDir = improc.dir.getDir(acqId, 1, 1);
+    case 'scopeRig'
         localScopeRigFolder = fullfile('\\User1-PC\D\data\Matthias\processed', acqId);
-        mkdir(localScopeRigFolder);
-        fprintf('Created new folder: %s\n', localScopeRigFolder);
-        acq.defaultDir = localScopeRigFolder;
+    case 'kristenPc'
+        localScopeRigFolder = fullfile('\\user-pc\C\data\Matthias\imaging\processed', acqId);
 end
+
+% Create local "processed" dir:
+mkdir(localScopeRigFolder);
+fprintf('Created new folder: %s\n', localScopeRigFolder);
+acq.defaultDir = localScopeRigFolder;
 
 % Motion correction parameters:
 acq.motionCorrectionFunction = @withinFile_withinFrame_lucasKanade;
