3261a31b298fdaa2a0b2adc9ce812e6467235711
mjlm-tarynspc <m.minderer@gmail.com>
2015-10-15 13:25:11 -0400

    Implement local temp storage with later upload to server in ajp

--- classes/@acq2pJobProcessor/processCurrentAcq.m
+++ classes/@acq2pJobProcessor/processCurrentAcq.m
@@ -2,7 +2,7 @@ function processCurrentAcq(ajp)
 % Performs all processing of currently loaded acquisition.
 
 %create cleanup obj
-cleanupObj = onCleanup(@() moveBackToUnproc(ajp));
+cleanupObj = onCleanup(@() cleanUpFun(ajp));
 
 if ajp.debug
     ajp.log('Skipping all processing because debug mode is on.');
@@ -59,7 +59,7 @@ else
 end
 
 % Caclulate pixel covariance:
-%check if pixel covariance already calculated
+% Check if pixel covariance already calculated
 if isempty(ajp.currentAcq.roiInfo) ...
         || (~isempty(pxCovRad) && ajp.currentAcq.roiInfo.slice(1).covFile.nh ~= (2*pxCovRad + 1))
     % ROI info does not exist or a different neighborhood size was
@@ -76,6 +76,40 @@ if isempty(ajp.currentAcq.roiInfo) ...
 else
     ajp.log('Covariance already calculated. Skipping...');
 end
+
+% Move files to remote directory:
+% We infer from the raw movie paths if the movies should uploaded to a
+% remote directory.
+if ~isempty(strfind(ajp.currentAcq.Movies{1}, 'research.files.med.harvard.edu')) ...
+        && isempty(strfind(ajp.currentAcq.correctedMovies.slice(1).channel(1).fileName{1}, 'research.files.med.harvard.edu'))
+    % The raw movies are on the server, but the corrected ones are not:
+    % --> Move corrected files to server.
+    
+    [dirRemoteRaw, ~, ~] = fileparts(ajp.currentAcq.Movies{1});
+    dirRemoteProcessed = strrep(dirRemoteRaw, '\raw\', '\processed\');
+    mkdir(dirRemoteProcessed);
+    
+    % Copy from local default dir to remote processed dir (using robocopy
+    % in the background):
+    % l - log only, don't actually copy.
+    % Z - resume if network connection lost.
+    % np - don't print % progress to console.
+    % xo - exclude older files.
+    % xd temp - exclude directories named "temp"
+    % & - run command in background.
+    % r - number of retries.
+    % s - copy subdirectories
+    copyCommand = 'robocopy %s %s /s /R:0 /Z /NP /log:%s & exit &';
+
+    % Copy local to server:
+    source = ajp.currentAcq.defaultDir;
+    destination = dirRemoteProcessed;
+    logFileName = fullfile(dirRemoteProcessed, 'robocopyLocalToServerLog.txt');
+    system(sprintf(copyCommand, source, destination, logFileName));
+    
+    ajp.log('Started robocopy to copy from local to remote "processed" directory.');
+end
+    
 
 % Move acqFile to done folder:
 if ~exist(ajp.dir.done, 'dir');
@@ -96,11 +130,16 @@ for ii = 1:numel(stack)
 end
 end
 
-function moveBackToUnproc(ajp)
+function cleanUpFun(ajp)
 %get error information
 errInfo = lasterror; %#ok<LERR>
+
 if isempty(errInfo.identifier)
     moveDest = fullfile(ajp.dir.jobs, ajp.currentAcqFileName);
+    movefile(fullfile(ajp.dir.inProgress, ajp.currentAcqFileName), moveDest);
+    msg = 'Exectuion terminated. Found no error, so assuming manual abort. Moving job file back to "to do" directory.';
+    ajp.log(msg);
+    return
 else
     moveDest = fullfile(ajp.dir.error, ajp.currentAcqFileName);
 end
@@ -109,8 +148,7 @@ if exist(fullfile(ajp.dir.inProgress, ajp.currentAcqFileName),'file')
     if ~exist(ajp.dir.error, 'dir');
         mkdir(ajp.dir.error);
     end
-    movefile(fullfile(ajp.dir.inProgress, ajp.currentAcqFileName),...
-        moveDest);
+    movefile(fullfile(ajp.dir.inProgress, ajp.currentAcqFileName), moveDest);
     msg = 'Exectuion terminated. Moved file to error folder.';
     ajp.log(msg);    
 end
