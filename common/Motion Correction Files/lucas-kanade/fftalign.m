902e0ef5eca89a40c1312f303bb0478bce75183b
mjlm-office <m.minderer@gmail.com>
2016-04-16 19:38:45 -0400

    Improve motion correction
    Code was added to create a more accurate reference image by applying a
    fast full-frame alignment before Lucas Kanade. Also, code was added to
    perform full affine registration of individual 1000-frame chunks to the
    global reference.

--- common/Motion Correction Files/lucas-kanade/fftalign.m
+++ common/Motion Correction Files/lucas-kanade/fftalign.m
@@ -1,17 +1,17 @@
 function [u,v] = fftalign(A,B)
 % [u,v] = fftalign(A,B)
 % From http://scanbox.wordpress.com/2014/03/20/recursive-image-alignment-and-statistics/
-N = min(size(A));
+[h, w] = size(A);
  
-yidx = round(size(A,1)/2)-N/2 + 1 : round(size(A,1)/2)+ N/2;
-xidx = round(size(A,2)/2)-N/2 + 1 : round(size(A,2)/2)+ N/2;
+% yidx = round(size(A,1)/2)-N/2 + 1 : round(size(A,1)/2)+ N/2;
+% xidx = round(size(A,2)/2)-N/2 + 1 : round(size(A,2)/2)+ N/2;
  
-A = A(yidx,xidx);
-B = B(yidx,xidx);
+% A = A(yidx,xidx);
+% B = B(yidx,xidx);
  
 C = fftshift(real(ifft2(fft2(A).*fft2(rot90(B,2)))));
 [~,i] = max(C(:));
 [ii, jj] = ind2sub(size(C),i);
  
-u = N/2-ii;
-v = N/2-jj;
+u = round(h/2-ii);
+v = round(w/2-jj);
--- common/Motion Correction Files/withinFile_withinFrame_lucasKanade.m
+++ common/Motion Correction Files/withinFile_withinFrame_lucasKanade.m
@@ -6,13 +6,14 @@ function movStruct = withinFile_withinFrame_lucasKanade(...
 
 nSlice = numel(movStruct.slice);
 nChannel = numel(movStruct.slice(1).channel);
-isGpu = gpuDeviceCount>0;
+isGpu = false;
 if isGpu
     gpu = gpuDevice;
     pctRunOnAll reset(gpuDevice);
     wait(gpu)
     memAvailable = gpu.AvailableMemory;
 end
+isEqualizeLuminace = true;
 
 switch opMode
     case 'identify'
@@ -21,39 +22,74 @@ switch opMode
         for iSl = 1:nSlice
             [h, w, z] = size(double(movStruct.slice(iSl).channel(refCh).mov));
             
-            % First, find within-movie displacements:
-            ref = mean(single(movStruct.slice(iSl).channel(refCh).mov), 3);
-            [movTemp, dpx, dpy, basisFunctions] = ...
-                doLucasKanadeSPMD(single(movStruct.slice(iSl).channel(refCh).mov), ref);
-                        
-            % Second, find global displacement with respect to reference
-            % image:
-            if movNum == refMov
-                xGlobal = 0;
-                yGlobal = 0;
-                obj.motionRefImage.slice(iSl).img = nanmedian(movTemp, 3);
-            else
-                medianThis = nanmedian(movTemp, 3);
-                [xGlobal, yGlobal] = track_subpixel_wholeframe_motion_fft_forloop(...
-                    medianThis, ...
-                    obj.motionRefImage.slice(iSl).img);
+            % Perform luminance equalization (this may be helpful for
+            % movies with very uneven brightness, e.g. one highly
+            % overexpressing region besides a healthy region of interest):
+            if isEqualizeLuminace
+                localLuminance = imgaussfilt(movStruct.slice(iSl).channel(refCh).mov, round(h/12));
+                movStruct.slice(iSl).channel(refCh).mov = ...
+                    bsxfun(@rdivide, movStruct.slice(iSl).channel(refCh).mov, localLuminance);
             end
             
-            % Third, combine local and global shifts and store them as
-            % displacement fields. A displacement field specifies how each
-            % pixel is shifted in each dimension.
-            % THIS IS NOW DONE IN THE FUNCTIONS BELOW TO SAVE STORAGE SPACE.
-%             lineShiftX = zeros(h, z);
-%             lineShiftY = zeros(h, z);
-%             for f = 1:z
-%                 lineShiftX(:, f) = basisFunctions*dpx(:, f) - xGlobal;
-%                 lineShiftY(:, f) = basisFunctions*dpy(:, f) - yGlobal;
-%             end
+            % Perform whole-frame translation correction (this is fast and
+            % improves the quality of the reference used for the Lucas
+            % Kanade step):
+            isEdge = (1:w) < 0.1*w | (1:w) > 0.9*w; % Exclude black bars!
+            r = detectFullframeRecursive(movStruct.slice(iSl).channel(refCh).mov(:, ~isEdge, :), 1:z);
+            movStruct.slice(iSl).channel(refCh).mov = ...
+                alignFullframe(movStruct.slice(iSl).channel(refCh).mov, r.T(:, 2), r.T(:, 1));
+            
+            % Calculate reference on full-frame-corrected movie:
+            ref = mean(movStruct.slice(iSl).channel(refCh).mov, 3);
+            ref = single(ref);
+            
+            % Find within-frame displacements:
+            [movTemp, dpx, dpy, basisFunctions] = ...
+                doLucasKanadeSPMD(single(movStruct.slice(iSl).channel(refCh).mov), ref);
+            
+            % Combine full-frame and within-frame displacements:
+            dpx = bsxfun(@minus, dpx, r.T(:, 2)');
+            dpy = bsxfun(@minus, dpy, r.T(:, 1)');
+                        
+            % Find global displacement with respect to global reference
+            % image:
+            if movNum == refMov
+                tformGlobal = affine2d;
+                obj.motionRefImage.slice(iSl).img = nanmedian(movTemp, 3);
+            else
+                refHere = nanmedian(movTemp, 3);
+                refGlobal = obj.motionRefImage.slice(iSl).img;
+                
+                % It is important to exclude the black bars that are
+                % present in older Scanimage files because they don't
+                % translate and will bias the results. We simply cut off
+                % 10 % on either side:
+                isEdge = (1:w) < 0.1*w | (1:w) > 0.9*w;
+                
+                % First, we find translation only. The subsequent affine
+                % step only works robustly if the translation has already
+                % been removed:
+                [xTrans, yTrans] = track_subpixel_wholeframe_motion_fft_forloop(...
+                    refHere(:, ~isEdge), refGlobal(:, ~isEdge));
+                refHereTransl = alignFullframe(refHere, xTrans, yTrans);
+                
+                % Find affine transformation using mutual information
+                % metric:
+                opt = imregconfig('Monomodal');
+                met = registration.metric.MattesMutualInformation;
+                RA = imref2d([h, sum(~isEdge)]);      
+                tformGlobal = imregtform(refHereTransl(:, ~isEdge), RA, refGlobal(:, ~isEdge), RA, ...
+                    'affine', opt, met, ...
+                    'InitialTransformation', affine2d, 'PyramidLevels', 1);
+                
+                % Combine translation and affine transformation:
+                tformGlobal.T = tformGlobal.T * [1 0 0; 0 1 0; xTrans yTrans 1];
+            end
             
             % The motion correction algorithm calculates a rigid shift for
             % each line (because lines are scanned fast, so there is
             % probably no shift within a line). The displacement field for
-            % the entrie frame is created by expanding the line shifts
+            % the entire frame is created by expanding the line shifts
             % across the entire frame (as if multiplying them with a
             % meshgrid). However, saving them in full matrices would
             % require as much space as the movie itself. Therefore, we
@@ -72,8 +108,8 @@ switch opMode
             % same time, they add values corresponding to the
             % grid-positions of each pixel (same as if we created grid
             % matrixes with MESHGRID, but more efficient).
-            obj.shifts(movNum).slice(iSl).x = createDispFieldFunctionX(h, w, z, basisFunctions, dpx, xGlobal);
-            obj.shifts(movNum).slice(iSl).y = createDispFieldFunctionY(h, w, z, basisFunctions, dpy, yGlobal);
+            obj.shifts(movNum).slice(iSl).x = createDispFieldFunctionX(h, w, z, basisFunctions, dpx, dpy, tformGlobal);
+            obj.shifts(movNum).slice(iSl).y = createDispFieldFunctionY(h, w, z, basisFunctions, dpx, dpy, tformGlobal);
         end
         
     case 'apply'
@@ -110,24 +146,38 @@ end
 % putting them into separate functions, we can isolate them from all the
 % larger movie-related variables that are in the workspace of the main
 % function:
-function fn = createDispFieldFunctionX(h, w, z, basisFunctions, dpx, xGlobal)
+function fn = createDispFieldFunctionX(h, w, z, basisFunctions, dpx, dpy, tformGlobal)
 fn = @dispFieldX;
     function dx = dispFieldX
+        % Get local displacement field:
         lineShiftX = zeros(h, z);
+        lineShiftY = zeros(h, z);
         for f = 1:z
-            lineShiftX(:, f) = basisFunctions*dpx(:, f) - xGlobal;
+            lineShiftX(:, f) = basisFunctions*dpx(:, f);
+            lineShiftY(:, f) = basisFunctions*dpy(:, f);
         end
         dx = bsxfun(@plus, 1:w, permute(lineShiftX, [1, 3, 2]));
+        dy = bsxfun(@plus, zeros(1, w), permute(bsxfun(@plus, (1:h)', lineShiftY), [1, 3, 2]));
+        
+        % Add global transformation:
+        [dx, ~] = transformPointsInverse(tformGlobal, dx, dy);
     end
 end
 
-function fn = createDispFieldFunctionY(h, w, z, basisFunctions, dpy, yGlobal)
+function fn = createDispFieldFunctionY(h, w, z, basisFunctions, dpx, dpy, tformGlobal)
 fn = @dispFieldY;
     function dy = dispFieldY
+        % Get local displacement field:
+        lineShiftX = zeros(h, z);
         lineShiftY = zeros(h, z);
         for f = 1:z
-            lineShiftY(:, f) = basisFunctions*dpy(:, f) - yGlobal;
+            lineShiftX(:, f) = basisFunctions*dpx(:, f);
+            lineShiftY(:, f) = basisFunctions*dpy(:, f);
         end
+        dx = bsxfun(@plus, 1:w, permute(lineShiftX, [1, 3, 2]));
         dy = bsxfun(@plus, zeros(1, w), permute(bsxfun(@plus, (1:h)', lineShiftY), [1, 3, 2]));
+        
+        % Add global transformation:
+        [~, dy] = transformPointsInverse(tformGlobal, dx, dy);
     end
 end
