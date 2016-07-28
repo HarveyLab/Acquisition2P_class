023db1d4b159f4cc1444653184604fdb2f297eb5
Selmaan <chettih@fas.harvard.edu>
2016-07-28 10:41:48 -0400

    slice support for viewAcq

--- personal/Selmaan/viewAcq.m
+++ personal/Selmaan/viewAcq.m
@@ -1,11 +1,15 @@
-function [mov, mF] = viewAcq(obj)
+function [mov, mF] = viewAcq(obj,nSlice)
 
-movStruct = obj.correctedMovies.slice(1).channel(1);
+if ~exist('nSlice','var')
+    nSlice = 1;
+end
+
+movStruct = obj.correctedMovies.slice(nSlice).channel(1);
 
 nMovies = length(movStruct.fileName);
 mov = nan([movStruct.size(1,1:2),nMovies]);
 for nMovie = 1:nMovies
-    t = meanRef(obj,nMovie);
+    t = meanRef(obj,nMovie,nSlice);
     mov(:,:,nMovie) = imNorm(t);
     mF(nMovie) = nanmean(t(:));
 end
