1024caa15e63dce1b33935b7c209561996cbc753
Selmaan <chettih@fas.harvard.edu>
2017-03-03 12:07:14 -0500

    add absShiftThresh to LucasKanadeSPMD

--- common/Motion Correction Files/lucas-kanade/doLucasKanadeSPMD.m
+++ common/Motion Correction Files/lucas-kanade/doLucasKanadeSPMD.m
@@ -175,7 +175,8 @@ function [Id, dpx, dpy, ii] = doLucasKanade_singleFrame(...
 
     warning('off','fastBSpline:nomex');
     maxIters = 50;
-    deltacorr = 0.0005;
+    deltacorr = 5e-4;
+    absShiftThresh = 1/3;
     [~, w] = size(T);
     
     %Find optimal image warp via Lucas Kanade    
@@ -214,6 +215,9 @@ function [Id, dpx, dpy, ii] = doLucasKanade_singleFrame(...
         dpx = dpx + Hx\gx;
         dpy = dpy + Hy\gy;
         
+        if max([dpx;dpy]) < absShiftThresh
+            break
+        end
         % no damping
 %         dpx = dpx + damping*dpx_;
 %         dpy = dpy + damping*dpy_;
