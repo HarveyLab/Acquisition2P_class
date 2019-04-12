0286cdda1da0d5de5329b0c78405d36bb460209e
AnnaJaffe-desktop <annawjaffe@gmail.com>
2019-04-12 14:52:48 -0400

    AJ motion correction function

---
+++ personal/Selmaan/AJlk.m
@@ -0,0 +1,36 @@
+function AJlk(obj)
+%Example of an Acq2P Initialization Function. Allows user selection of
+%movies to form acquisition, sorts alphabetically, assigns an acquisition
+%name and default directory, and assigns the object to a workspace variable
+%named after the acquisition name
+
+%Initialize user selection of multiple tif files
+[movNames, movPath] = uigetfile('E:\Anna\*.tif','MultiSelect','on');
+
+%Set default directory to folder location,
+obj.defaultDir = movPath;
+
+%sort movie order alphabetically for consistent results
+movNames = sort(movNames);
+
+%Attempt to automatically name acquisition from movie filename, raise
+%warning and create generic name otherwise
+obj.acqName = 'FOV1';
+
+%Attempt to add each selected movie to acquisition in order
+for nMov = 1:length(movNames)
+    obj.addMovie(fullfile(movPath,movNames{nMov}));
+end
+
+%Automatically fill in fields for motion correction
+obj.motionRefMovNum = 10;
+obj.motionRefChannel = 1;
+obj.binFactor = 1;
+% obj.motionCorrectionFunction = @lKFast_affineSlow_memMap;
+obj.motionCorrectionFunction = @lucasKanade_plus_nonrigid_memMap;
+
+%Assign acquisition object to acquisition name variable in workspace
+assignin('base',obj.acqName,obj);
+
+%Notify user of success
+fprintf('Successfully added %03.0f movies to acquisition: %s\n',length(movNames),obj.acqName),
